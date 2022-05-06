df6f9fa32b1d6b8f21ba8c89aa6e1d7d7cce42b0
unknown <gandswaja@gmail.com>
2022-05-06 11:26:27 +0700

    udapte

--- my-watch/src/pages/home.jsx
+++ my-watch/src/pages/home.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import Axios from 'axios'
+import {
+    Carousel,
+    Card,
+    Button
+} from 'react-bootstrap'
 import NavigationBar from '../component/navigationBar'
 
 class HomePage extends React.Component {
@@ -10,17 +16,164 @@ class HomePage extends React.Component {
         }
     }
 
-   
+    componentDidMount() {
+        Axios.get('http://localhost:2000/slider')
+            .then(res => {
+                this.setState({ carousels: res.data })
+                Axios.get('http://localhost:2000/products')
+                    .then(res => {
+                        this.setState({ products: res.data })
+                    })
+            })
+    }
 
     render() {
+        console.log(this.state.carousels)
+        console.log(this.state.products)
         return (
-            <div>
+            <div style={{ backgroundColor: '#A3DDCB', }}>
                 <NavigationBar />
+                <div style={styles.container}>
+                    <Carousel style={styles.carousel}>
+                        {this.state.carousels.map(item => {
+                            return (
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block"
+                                        src={item.image}
+                                        alt="First slide"
+                                        style={{ width: '90vw', height: '90vh' }}
+                                    />
+                                    <Carousel.Caption style={styles.caroCaption}>
+                                        <h2>{item.title}</h2>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                            )
+                        })}
+                    </Carousel>
+                    <div style={styles.sectProducts}>
+                        <h1>Our Products</h1>
+                        <div style={styles.contProducts}>
+                            {this.state.products.map(item => {
+                                console.log(item.name.length)
+                                return (
+                                    <Card style={{ width: '18rem', marginBottom: '15px', marginTop: '15px' }}>
+                                        <Card.Img style={styles.cardImg} variant="top" src={item.images[0]} />
+                                        <Card.Body style={styles.cardBody}>
+                                            <Card.Title style={styles.cardTitle}>{item.brand}  - {item.name} </Card.Title>
+                                            <Card.Text><strong>IDR {item.price.toLocaleString()}</strong></Card.Text>
+                                            <div style={styles.contButton}>
+                                                <Button variant="outline-light"><i class="far fa-bookmark"></i></Button>
+                                                <Button variant="outline-light">
+                                                    <i class="fas fa-cart-plus"></i> Buy Now
+                                                </Button>
+                                            </div>
+                                        </Card.Body>
+                                    </Card>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+                <footer style={styles.footer}>
+                    <div style={styles.footerList}>
+                        <div style={styles.footerItem}>
+                            <h6 style={styles.footerItemh6}>Product</h6>
+                            <a href="#">Download</a>
+                            <a href="#">Pricing</a>
+                            <a href="#">Locations</a>
+                        </div>
+                        <div style={styles.footerItem}>
+                            <h6 style={styles.footerItemh6}>Engage</h6>
+                            <a href="#">FAQ</a>
+                            <a href="#">Tutorials</a>
+                            <a href="#">About Us</a>
+                        </div>
+                        <div style={styles.footerItem}>
+                            <h6 style={styles.footerItemh6}>Earn Money</h6>
+                            <a href="">Become Partners</a>
+                        </div>
+                    </div>
+                    <div>
+                        <h6 style={{ textAlign: 'center', margin: '0' }}>Copyright@Gandswaja</h6>
+                    </div>
+                </footer>
             </div>
         )
     }
 }
 
+const styles = {
+    container: {
+        marginTop: '7vh',
+        paddingTop: '3vh'
+    },
+    carousel: {
+        height: '90vh',
+        width: '90vw',
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    },
+    caroCaption: {
+        backgroundColor: 'rgba(0, 0, 0, .5)',
+        marginBottom: '5%',
+        width: '45%',
+        right: '0',
+        left: '0',
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    },
+    sectProducts: {
+        marginTop: '30px',
+        marginLeft: '5vw',
+        marginRight: '5vw',
+        borderTop: '1px solid black',
+        paddingTop: '20px',
+    },
+    contProducts: {
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        flexWrap: 'wrap',
+        borderRadius: "10px"
+    },
+    cardImg: {
+        padding: '15px',
+        width: '20vw',
+        height: '35vh'
+    },
+    cardBody: {
+        backgroundColor: '#03506F',
+        borderRadius: '15px 15px 0 0',
+        color: '#f8f9fa'
+    },
+    cardTitle: {
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap'
+    },
+    contButton: {
+        display: 'flex',
+        justifyContent: 'space-around'
+    },
+    footer: {
+        backgroundColor: "#FFE3DE",
+    },
+    footerList: {
+        display: "flex",
+        justifyContent: "space-around",
+        padding: "100px 0",
+        marginTop: "15px"
+    },
+    footerItem: {
+        display: "flex",
+        flexDirection: "column",
+    },
+    footerItemh6: {
+        fontWeight: "600",
+        fontSize: "18px",
+        margin: "0",
+        marginBottom: "10px",
+    },
+}
 
-   
 export default HomePage
